f0f0bb9bd07eb701ab54526e04120ca7c134525f
thanhnt <thanhnt@vccloud.vn>
2014-05-29 17:37:28 +0700

    add comment code

--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
 import os
 
 def fix_path(filename):
+    """ return path of filename
+    """
     #because this app run with crontab, it must be use abs path
+    # return the canonical path of file (__file__)
     filepath = os.path.realpath(__file__)
+    # return directory contain filepath
     path = os.path.dirname(filepath)
     fixed = os.path.join(path, filename)
     return fixed
